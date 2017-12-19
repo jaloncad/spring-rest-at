c5fee4c1c37f4c1ff35f99b28b7f969f5588c23e
Juan Alonso Cadierno <juan.alonso@thinkd.io>
2017-12-19 19:01:39 +0000

    Initial commit from Atomist

---
+++ src/main/java/firstatomist/SpringRestAtApplication.java
@@ -0,0 +1,12 @@
+package firstatomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestAtApplication.class, args);
+	}
+}
